b4943d452abfb59d1d1008ba14e0963c8a19d007
Mahmoud Eraky <mahmoudheraky@gmail.com>
2023-03-30 14:53:32 +0200

    Added Comments

--- src/main/java/com/example/demo/FizzBuzzApplication.java
+++ src/main/java/com/example/demo/FizzBuzzApplication.java
@@ -16,21 +16,25 @@ public class FizzBuzzApplication {
 		SpringApplication.run(FizzBuzzApplication.class, args);
 	}
 
-	@GetMapping("/FizzBuzz")
+	@GetMapping("/FizzBuzz") // mapping to the fizzbuzz function
 	public ObjectNode hello(@RequestParam(value = "eingabe", defaultValue = "225") Integer eingabe) {
+		// creating the json object
 		ObjectMapper mapper = new ObjectMapper();
 		ObjectNode ausgabe = mapper.createObjectNode();
+
+		// iterating through the numbers up to the param eingabe
 		for (int i = 1; i <= eingabe; i++) {
 			String tmp = "";
 			if (i % 3 == 0) {
-				tmp = "Fizz";
+				tmp = "Fizz"; // if i dividable by 3 add 'fizz' to tmp
 			}
 			if (i % 5 == 0) {
-				tmp += "Buzz";
+				tmp += "Buzz"; // if i dividable by 5 add 'buzz' to tmp
 			}
-			ausgabe.put(String.valueOf(i), tmp);
+			ausgabe.put(String.valueOf(i), tmp); // add the number and the result stored in tmp to the json object
+													// ausgabe
 		}
-		return ausgabe;
+		return ausgabe; // return the json object
 	}
 
 }
